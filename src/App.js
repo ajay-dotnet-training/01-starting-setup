0ffda63367435d4c2d516823c41349efe0fe8b39
ajay-dotnet-training <ajaykumarn0704@gmail.com>
2024-01-31 12:18:55 +0530

    child to parent communication component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+//import ExpenseItem from "./components/ExpenseItem";
+import NewExpenses from "./components/NewExpenses/NewExpenses";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const  App = () => {
+  // const para = document.createElement('p');
+  // para.textContent = 'This is also visible ';
+  // document.getElementById('root').append(para);
+
+  const expenses = [
+    {
+      id: "e1",
+      title: "WireLess KeyBoard and Mouse",
+      amount: 94.12,
+      date: new Date(2023, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2024, 1, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2023, 5, 12),
+    },
+  ];
+
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h2", {}, "Let's Get Started!"),
+  //   React.createElement(Expenses,{items:expenses})
+  // );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewExpenses />
+      <Expenses items={expenses} />
     </div>
   );
 }
